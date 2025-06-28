1a70fd2347ed93d61e3bcb74729350ced5074adb
CadeMichael <cade.lueker@gmail.com>
2025-06-28 02:39:27 +0200

    new lsp

--- lua/flix/lsp.lua
+++ lua/flix/lsp.lua
@@ -1,40 +1,16 @@
 local M = {}
 
-function M.set_config()
-  require("lspconfig.configs").flix = {
-    default_config = {
+function M.setup()
+  print(vim.fs.root(0, { 'flix.toml' }))
+  if not vim.lsp.config["flix"] then
+    vim.lsp.config('flix', {
       cmd = { "java", "-jar", "flix.jar", "lsp" },
       filetypes = { "flix" },
-      root_dir = function(fname)
-        local root_dir = vim.fs.dirname(vim.fs.find({ "flix.toml", "flix.jar" }, { path = fname, upward = true })[1])
-            or vim.fs.dirname(fname)
-        local flix_jar_path = vim.fs.joinpath(root_dir, "flix.jar")
-        if vim.loop.fs_stat(flix_jar_path) == nil then
-          print("Failed to start the LSP server: flix.jar not found in project root (" .. root_dir .. ")!\n")
-          return nil
-        end
-        return root_dir
-      end,
-      settings = {},
-    },
-  }
-end
-
-function M.flix_lsp(c, a)
-  require("lspconfig").flix.setup {
-    capabilities = c,
-    on_attach = function(_, bufnr)
-      -- flix specific pre attatch actions
-      print("Flix LSP attached to buffer " .. bufnr)
-      vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
-        pattern = "<buffer>",
-        callback = function()
-          vim.lsp.codelens.refresh({ bufnr = bufnr })
-        end,
-      })
-      a(_, bufnr)
-    end,
-  }
+      root_markers = { "flix.toml" },
+      cmd_cwd = vim.fs.root(0, { 'flix.toml' }),
+      root_dir = vim.fs.root(0, { 'flix.toml' }),
+    })
+  end
 end
 
 return M
--- plugin/flix.lua
+++ plugin/flix.lua
@@ -6,12 +6,23 @@ vim.filetype.add({
 })
 
 -- flix language defaults
+local flix = vim.api.nvim_create_augroup("flix", { clear = true })
+local flix_lsp = vim.api.nvim_create_augroup("flix", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
+  group = flix,
   pattern = "flix",
-  callback = function()
+  callback = function(args)
+    vim.api.nvim_clear_autocmds({group = flix_lsp})
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
     vim.opt_local.softtabstop = 4
     vim.bo.commentstring = "// %s"
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
+      group = flix_lsp,
+      pattern = "<buffer>",
+      callback = function()
+        vim.lsp.codelens.refresh({ bufnr = args.buf })
+      end
+    })
   end
 })
